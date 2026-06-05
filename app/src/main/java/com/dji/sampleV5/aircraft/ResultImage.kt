39778967fc5a9f6518fe93a3483c61e684bb6cd8
FenixTags <josedavid.jdss777@gmail.com>
2026-06-05 11:04:47 -0600

    Experimental blue filter using HSV and centroid adquisition using CMSS algorithm.

---
+++ app/src/main/java/com/dji/sampleV5/aircraft/ResultImage.kt
@@ -0,0 +1,17 @@
+package com.dji.sampleV5.aircraft
+
+import android.graphics.Bitmap
+
+/**
+ * Project: sampleV5aircraft
+ * From: com.dji.sampleV5.aircraft
+ * Created by: FenixTags
+ * On 6/3/2026
+ * All Rights Reserved 2026
+ */
+data class ResultImage(val bitmap: Bitmap, val currentFps:Int, val filterTimeMs: Long)
+
+data class CoordsCMSS(val xCMSS: Int?, val yCMSS: Int?,
+                      val bitmap: Bitmap,
+                      val currentFps:Int, val filterTimeMs: Long // Variables de rendimiento
+                    )
